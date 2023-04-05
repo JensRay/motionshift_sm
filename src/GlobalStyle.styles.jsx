e8bc35a00307840703a28d7afd0319fccf7d0089
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2023-04-05 10:43:22 +0200

    fix: color link style

--- src/GlobalStyle.styles.jsx
+++ src/GlobalStyle.styles.jsx
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
-import { colors } from "./utilities/_variables.styles";
-
 export const GlobalStyle = createGlobalStyle`
   a {
     text-decoration: none;
     &:visited {
-      color: ${colors.navy_blue};
+      color: inherit;
     }
   }
 `;
